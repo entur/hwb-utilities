e0021a2815ebb46eca518fe80d905613f58db18e
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2025-11-14 15:48:42 +0100

    Split generation into two

---
+++ hwb-messages-android-lib/src/test/java/hwb/DummyTest.java
@@ -0,0 +1,12 @@
+package hwb;
+
+import org.junit.Test;
+// https://issuetracker.google.com/issues/411739086?pli=1#comment4
+public class DummyTest {
+
+    @Test
+    public void test() {
+        // do nothing
+    }
+
+}
